41fe1f4b9f40e503009496aedfe5a482d58e5be7
Haziq Hapiz <zyqq.dev@gmail.com>
2023-05-27 19:34:00 +0800

    refactor the Axios POST request

--- client/src/components/GenerateReceipt.vue
+++ client/src/components/GenerateReceipt.vue
@@ -18,9 +18,10 @@ export default {
   },
   methods: {
     save() {
-      axios.post("http://127.0.0.1:8000/api/save", this.details)
+      axios.post("http://localhost:3000/generateReceipt", JSON.stringify(this.details), { headers: { 'Content-Type': 'application/json' } })
         .then(
           ({ data }) => {
+            console.log(data.receipt)
             alert("Receipt generated.");
           }
         )
@@ -30,23 +31,25 @@ export default {
 </script>
 
 <template>
-  <h1>Bank Receipt Generation</h1>
+  <div>
+    <h1>Bank Receipt Generation</h1>
 
-  <form @submit.prevent="save">
-    <p>From Account: <input v-model="details.senderNoAccount" /></p>
-    <p>To Account: <input v-model="details.receiverNoAccount" /></p>
-    <p>Account Holder Name: <input v-model="details.receiverName" /></p>
-    <div>Transaction Type:</div>
-    <input type="radio" id="one" value="IBG Transfer" v-model="details.transactionType" />
-    <label for="one">IBG Transfer</label> <br>
-    <input type="radio" id="two" value="Instant Transfer" v-model="details.transactionType" />
-    <label for="two">Instant Transfer</label>
-    <p>Amount: <input v-model="details.amount" placeholder="RM" /></p>
-    <p>Reference 1: <input v-model="details.ref1" /></p>
-    <p>Reference 2(optional): <input v-model="details.ref2" /></p>
+    <form @submit.prevent="save">
+      <p>From Account: <input v-model="details.senderNoAccount" /></p>
+      <p>To Account: <input v-model="details.receiverNoAccount" /></p>
+      <p>Account Holder Name: <input v-model="details.receiverName" /></p>
+      <div>Transaction Type:</div>
+      <input type="radio" id="one" value="IBG" v-model="details.transactionType" />
+      <label for="one">IBG Transfer</label> <br>
+      <input type="radio" id="two" value="Instant" v-model="details.transactionType" />
+      <label for="two">Instant Transfer</label>
+      <p>Amount: <input v-model="details.amount" placeholder="RM" /></p>
+      <p>Reference 1: <input v-model="details.ref1" /></p>
+      <p>Reference 2(optional): <input v-model="details.ref2" /></p>
 
-    <button type="submit" class="btn">Generate</button>
-  </form>
+      <button type="submit" class="btn">Generate</button>
+    </form>
+  </div>
 </template>
 
 <style scoped></style>
